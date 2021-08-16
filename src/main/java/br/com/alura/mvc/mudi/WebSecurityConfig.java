69cddf1e02c3788e6bb70b5a8e7f38c6aa9cee44
victor-murta <vmurtag16@gmail.com>
2021-08-16 18:59:51 -0300

    Java

--- src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -1,43 +1,71 @@
 package br.com.alura.mvc.mudi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
+
     //authorization
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
-              .authorizeRequests()
+            .authorizeRequests()
                 .anyRequest().authenticated()
-              .and()
+                .and()
                 .formLogin(form -> form
                         .loginPage("/login")
                         .permitAll()
-                );
+                )
+                .logout(logout -> logout.logoutUrl("/logout"));
     }
 
-    //authentication
-    @Bean
     @Override
-    public UserDetailsService userDetailsService(){
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
         UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("joao")
-                        .password("joao")
+                User.builder()
+                        .username("maria")
+                        .password(encoder.encode("maria"))
                         .roles("ADM")
                         .build();
 
-        return new InMemoryUserDetailsManager(user);
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(encoder)
+                .withUser(user);
     }
+
+
+    //authentication
+//    @Bean
+//    @Override
+//    public UserDetailsService userDetailsService(){
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("joao")
+//                        .password("joao")
+//                        .roles("ADM")
+//                        .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
 }
